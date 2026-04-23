b246adb963ee60e6103fa3f73e567ece2f26cdaf
Kerry Smart <kerry.z.smart@gmail.com>
2026-04-23 15:21:52 -0400

    calculate times from end time

--- modules/time.js
+++ modules/time.js
@@ -16,15 +16,11 @@ export function getInitialTime() {
     return workableTime;
 }
 
-//initTime is { start: Temporal.PlainTime, end: Temporal:PlainTime }
+// tasks is array of Tasks
+// initTime is { start: Temporal.PlainTime, end: Temporal:PlainTime }
  export function calculateTaskTime(tasks, initTime) {
     console.log("starting with:")
-    console.log(tasks);
-    console.log(initTime);
     let calcArray;
-    //check for start or end time
-    //startTime: add to get tasks
-
     if (initTime.start) {
         console.log("start time found: adding to calculate");
         return tasks.map((t, i) => {
@@ -32,26 +28,14 @@ export function getInitialTime() {
                      t.startTime = tasks[i-1].endTime;
             t.endTime = t.startTime.add(t.duration);
             return t;
-            //CURR - may need to refactor this?
-            //it works but it soooort of mutates the original array
-            //like not really but it updates the properties of the Tasks
-            //which idk is kind of fine and what I want 
-            //(plus they don't get saved in localStorage anyway)
-            //but is maybe not best practice
-            //but like....I do want to be able to access that information in script.js
-            //so like idk
-            //also the return function doesn't seem to matter here so idk
         });
     } else if (initTime.end) {
-        console.log("end time found: subtracting to calculate");
-        //CURR - working on this but i abruptly got too sleepy
+        return tasks.map((t,i) => {
+            i === 0? t.endTime = initTime.end : 
+                     t.endTime = tasks[i-1].startTime;
+            t.startTime = t.endTime.subtract(t.duration);
+            return t;
+        });
     }
-
-
-    //endTime: subtract to get tasks
-
-
-    console.log("ending with:")
-    console.log(calcArray);
     return calcArray;
 }
